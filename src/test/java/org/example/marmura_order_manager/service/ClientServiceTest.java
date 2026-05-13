4a3fbf3da48f7807b657bf09b4c2f6a234d19ffb
mariozcn <rusumario2411@gmail.com>
2026-05-13 21:51:35 +0300

    finished client service tests

--- src/test/java/org/example/marmura_order_manager/service/ClientServiceTest.java
+++ src/test/java/org/example/marmura_order_manager/service/ClientServiceTest.java
@@ -8,10 +8,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class ClientServiceTest {
     private ClientService clientService;
@@ -40,5 +40,44 @@ public class ClientServiceTest {
         assertEquals(1,result.size());
         assertEquals("Test",result.get(0).getName());
     }
+
+    @Test
+    void cautaClientDupaId(){
+        Client client1 = new Client();
+        client1.setName("test");
+        client1.setTelefon("07777777");
+        when(clientRepository.findById(1L)).thenReturn(Optional.of(client1));
+
+        Client rezultat = clientService.getClientById(1L);
+
+        assertEquals("test",rezultat.getName());
+        assertEquals("07777777",rezultat.getTelefon());
+    }
+
+    @Test
+    void updateClientTest(){
+        Client client = new Client();
+        client.setName("Nume vechi");
+
+        when(clientRepository.save(client)).thenReturn(client);
+
+        client.setName("Nume nou");
+        Client rezultat = clientService.salveazaClient(client);
+
+        assertEquals("Nume nou",rezultat.getName());
+    }
+
+    @Test
+    void stergeClientTest(){
+        Client client = new Client();
+        client.setName("Test");
+
+        when(clientRepository.findById(1L)).thenReturn(Optional.of(client));
+        when(comandaRepository.findComandaByClient(client)).thenReturn(List.of());
+
+        clientService.deleteClient(1L);
+        verify(clientRepository).deleteById(1L);
+    }
+
 
 }
